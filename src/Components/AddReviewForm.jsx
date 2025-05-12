7899cc69f8ef11370688478803c2b84f8e41d08a
Muhammad Ibrahim <Mhammad.Ibrahim@staples.com>
2025-05-12 19:58:36 +0500

    dummy reviews

--- src/Components/AddReviewForm.jsx
+++ src/Components/AddReviewForm.jsx
@@ -2,12 +2,50 @@ import { useState } from "react";
 import { motion } from "framer-motion";
 import { Camera } from "lucide-react";
 
+import { db, storage } from "../firebase";
+import { addDoc, collection } from "firebase/firestore";
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
+
 function AddReviewForm() {
   const [images, setImages] = useState([]);
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    number: "",
+    description: "",
+  });
+
+  const handleFormData = async () => {
+    try {
+      const imageURls = [];
+
+      for (const img of images) {
+        const imageRef = await ref(
+          storage,
+          `reviewImages/${Date.now()}-${img.name}`
+        );
+        await uploadBytes(imageRef, img);
+        const downloadUrl = await getDownloadURL(imageRef);
+        imageURls.push(downloadUrl);
+      }
+
+      const dataWithImages = { ...formData, images: imageURls };
+      const docRef = await addDoc(collection(db, "reviews"), dataWithImages);
+      console.log("Document ID:", docRef.id);
+    } catch (error) {
+      console.log("Error:", error);
+    }
+  };
+
+  const handleChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
   const handleImageUpload = (event) => {
-    const files = Array.from(event.target.files).slice(0, 5 - images.length);
-    const imageUrls = files.map((file) => URL.createObjectURL(file));
-    setImages((prevImages) => [...imageUrls, ...prevImages].slice(0, 5)); // Prepend new images
+    const selectedFiles = Array.from(event.target.files).slice(
+      0,
+      5 - images.length
+    );
+    setImages((prev) => [...selectedFiles, ...prev].slice(0, 5));
   };
   return (
     <motion.div
@@ -21,24 +59,32 @@ function AddReviewForm() {
       </h2>
       <motion.input
         type="text"
+        name="name"
         placeholder="Name"
         className="p-3 rounded-xl w-2/3 focus:outline-none leading-6 bg-background"
+        onChange={handleChange}
       />
       <motion.input
         type="email"
+        name="email"
         placeholder="Email"
         className="p-3 rounded-xl w-2/3 focus:outline-none leading-6 bg-background"
+        onChange={handleChange}
       />
       <motion.input
         type="tel"
+        name="number"
         placeholder="Phone Number"
         className="p-3 rounded-xl w-2/3 focus:outline-none leading-6 bg-background"
+        onChange={handleChange}
       />
       <motion.input
         // whileFocus={{ scale: 1.05 }}
         type="text"
+        name="description"
         placeholder="Please write you review here"
         className="p-3 rounded-xl w-2/3 focus:outline-none leading-6 bg-background"
+        onChange={handleChange}
       />
       <div className="flex flex-col items-center gap-2">
         <label className="cursor-pointer flex items-center gap-2 text-white px-4 py-2 rounded-xl text-primaryA bg-secondaryButton">
@@ -56,7 +102,7 @@ function AddReviewForm() {
             {images.map((img, index) => {
               const totalWidth = images.length * 20; // Total width occupied by images
               const centerOffset = totalWidth / 2; // Offset to center them
-
+              // const imgUrl = URL.createObjectURL(img);
               return (
                 <motion.img
                   key={index}
@@ -82,6 +128,7 @@ function AddReviewForm() {
       <motion.button
         whileTap={{ scale: 0.95 }}
         className="text-white p-3 rounded-4xl w-2/3 font-semibold bg-heading"
+        onClick={() => handleFormData()}
       >
         Submit
       </motion.button>
