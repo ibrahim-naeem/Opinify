0edc7d561e514fb95cce7e71ee20df3e35d25cb5
ibrahim-naeem <ibrahim22naeem@gmail.com>
2025-08-12 16:41:03 +0500

    login url changes

--- src/Pages/Email.jsx
+++ src/Pages/Email.jsx
@@ -20,7 +20,7 @@ function EmailSection() {
       email,
       options: {
         shouldCreateUser: true,
-        emailRedirectTo: "http://localhost:5173/review",
+        emailRedirectTo: "https://opinify.vercel.app/review",
       },
     });
     toast.success("Check your email for the magic link!");
